18f5dd509756431a5c226004713f27753b385296
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-08-05 16:45:36 +0300

    feat: extract extra info

--- src/search.py
+++ src/search.py
@@ -1,5 +1,5 @@
 import argparse
-from json import loads
+from json import dumps, loads
 from pathlib import Path
 
 from src.scraper import GitHub, RequestParams
@@ -10,7 +10,8 @@ class NoInputFileFoundError(Exception): ...
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="GitHub Search CLI")
-    parser.add_argument("input_file_path", type=str, help="A path to the file with input data.")
+    parser.add_argument("input_file_path", type=str, help="A path to the file with input data (JSON).")
+    parser.add_argument("output_file_path", type=str, help="A path to the file with parsed data (JSON).")
     args = parser.parse_args()
     input_file_path = Path(args.input_file_path)
     if not input_file_path.exists():
@@ -23,9 +24,16 @@ def main() -> None:
         type=input_data["type"],
     )
     github = GitHub()
-    html = github.search(params)
-    github.extract_urls(html)
+    html = github.request_page_html(url=f"{github.base_url}/search", proxies=params.proxies, params=params.to_dict())
+    urls = github.extract_urls(html)
+    for url in urls:
+        extra_info = github.extract_extra_info(url["url"], proxies=params.proxies)
+        if extra_info:
+            url["extra"] = extra_info  # pyright: ignore[reportArgumentType]
     github.close()
+    output_file_path = Path(args.output_file_path)
+    output_file_path.touch()
+    output_file_path.write_text(dumps(urls))
 
 
 if __name__ == "__main__":
